4c25172a7a708a025fa9ddb4d197d8985df4dba3
T_cat9000_2 <terpstragraham@gmail.com>
2026-05-29 17:23:37 -0700

    update api config and api

--- api/api.py
+++ api/api.py
@@ -3,14 +3,13 @@ from flask import Flask, jsonify
 # chatgpt cleaned this up, also i am prolly going to add more endpoints and make it easier to maintain, as well as mabye automate and make the website use this api to get the forecast too, which will make everything a but eassier.
 
 app = Flask(__name__)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 @app.route('/api/temperature')
 def temperature():
 
     return jsonify({
-        "temperature-high-lake-oswego": 80,
-        "temperature-low-lake-oswego": 54
+        "temperature-high-lake-oswego": 67,
+        "temperature-low-lake-oswego": 45
     })
 
 @app.route('/api/skies')
@@ -18,7 +17,7 @@ def skies():
 
     return jsonify({
         "skies-lake-oswego":
-        "cloudy until 4pm, then mostly cloudy with a chance of rain before 1am."
+        "Cloudy and rainy starting 8am and then clearing up around 3pm then partly cloudy skies from 8pm until the end of the day."
     })
 
 @app.route('/api/warnings-advisories')
