6bd4d63b47baa3f0fac3834729da6fc74cad74bc
T_cat9000_2 <terpstragraham@gmail.com>
2026-05-30 20:22:46 -0700

    updated LUT...
    updated LUT
    Added notes about special lunar event
    finished updates for the day
    gonna add some more dynamic stuff maybe before hopping off

--- api/api.py
+++ api/api.py
@@ -31,7 +31,7 @@ def alerts():
 @app.route('/api/last-updated')
 def last_updated():
     return jsonify({
-        "last-updated-lake-oswego": "May 30th, 2026 at 8:15 PM PT"
+        "last-updated-lake-oswego": "May 30th, 2026 at 8:25 PM PT"
     })
 
 @app.route('/api/observations')
