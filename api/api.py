27ab33e3d7c91bef065787567ee1575092c58c23
T_cat9000_2 <terpstragraham@gmail.com>
2026-05-30 17:55:33 -0700

    updates

--- api/api.py
+++ api/api.py
@@ -31,7 +31,7 @@ def alerts():
 @app.route('/api/last-updated')
 def last_updated():
     return jsonify({
-        "last-updated-lake-oswego": "May 30th, 2026 at 5:50 PM"
+        "last-updated-lake-oswego": "May 30th, 2026 at 5:55 PM PT"
     })
 
 @app.route('/api/observations')
