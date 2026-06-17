cf629162cee021e19365913604bd252ef2810ffa
T_cat9000_2 <terpstragraham@gmail.com>
2026-06-16 18:10:48 -0700

    oops misssed a day

--- api/api.py
+++ api/api.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 # chatgpt cleaned this up, also i am prolly going to add more endpoints and make it easier to maintain, as well as mabye automate and make the website use this api to get the forecast too, which will make everything a but eassier.
 
-last_updated = "June 14th, 2026 at 5:45 PM PT"
-temp_last_updated = "June 14th, 2026 at 5:45 PM PT"
-observations_last_updated = "June 14th, 2026 at 5:45 PM PT"
+last_updated = "June 16th, 2026 at 5:45 PM PT"
+temp_last_updated = "June 16th, 2026 at 5:45 PM PT"
+observations_last_updated = "June 16th, 2026 at 5:45 PM PT"
 
 app = Flask(__name__)
 
@@ -12,8 +12,8 @@ app = Flask(__name__)
 def temperature():
 
     return jsonify({
-        "temperature-high-lake-oswego": 95,
-        "temperature-low-lake-oswego": 65,
+        "temperature-high-lake-oswego": 85,
+        "temperature-low-lake-oswego": 54,
         "temperature-last-updated-lake-oswego": temp_last_updated
     })
 
@@ -22,7 +22,7 @@ def skies():
 
     return jsonify({
         "skies-lake-oswego":
-        "Sunny."
+        "Mostly Sunny."
     })
 
 @app.route('/api/alerts')
@@ -42,7 +42,7 @@ def last_updated():
 @app.route('/api/observations')
 def observations():
     return jsonify({
-        "observations-lake-oswego": "91 degrees Fahrenheit, Sunny.",
+        "observations-lake-oswego": "78 degrees Fahrenheit, Mostly sunny.",
         "last-updated-observations-lake-oswego": observations_last_updated
     })
 
