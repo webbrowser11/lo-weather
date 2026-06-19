03681aa1c6a3ddd78e5a96fa82eed7a8d09a0a15
T_cat9000_2 <terpstragraham@gmail.com>
2026-06-19 15:20:49 -0700

    dkddy

--- api/api.py
+++ api/api.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 # chatgpt cleaned this up, also i am prolly going to add more endpoints and make it easier to maintain, as well as mabye automate and make the website use this api to get the forecast too, which will make everything a but eassier.
 
-last_updated = "June 18th, 2026 at 4:30 PM PT"
-temp_last_updated = "June 18th, 2026 at 4:30 PM PT"
-observations_last_updated = "June 18th, 2026 at 4:30 PM PT"
+last_updated = "June 19th, 2026 at 3:20 PM PT"
+temp_last_updated = "June 19th, 2026 at 3:20 PM PT"
+observations_last_updated = "June 19th, 2026 at 3:20 PM PT"
 
 app = Flask(__name__)
 
@@ -12,8 +12,8 @@ app = Flask(__name__)
 def temperature():
 
     return jsonify({
-        "temperature-high-lake-oswego": 87,
-        "temperature-low-lake-oswego": 59,
+        "temperature-high-lake-oswego": 89,
+        "temperature-low-lake-oswego": 55,
         "temperature-last-updated-lake-oswego": temp_last_updated
     })
 
@@ -22,7 +22,7 @@ def skies():
 
     return jsonify({
         "skies-lake-oswego":
-        "Cloudy."
+        "Sunny."
     })
 
 @app.route('/api/alerts')
@@ -30,7 +30,7 @@ def alerts():
 
     return jsonify({
         "alerts-lake-oswego":
-        "AN EXTREME DROUGHT WATCH CONTINUES FOR THE ENTIRE STATE OF OREGON."
+        "AN EXTREME HEAT WATCH IS IN EFFECT FROM JUNE 22, 11:00 AM PDT UNTIL JUNE 23, 11:00 PM PDT\nAN EXTREME DROUGHT WATCH CONTINUES FOR THE ENTIRE STATE OF OREGON."
     })
 
 @app.route('/api/last-updated')
@@ -42,7 +42,7 @@ def last_updated():
 @app.route('/api/observations')
 def observations():
     return jsonify({
-        "observations-lake-oswego": "82 degrees Fahrenheit, Cloudy.",
+        "observations-lake-oswego": "84 degrees Fahrenheit, Sunny.",
         "last-updated-observations-lake-oswego": observations_last_updated
     })
 
