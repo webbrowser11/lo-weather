a47091edd0485a490916d3bd37904dd551d22611
T_cat9000_2 <terpstragraham@gmail.com>
2026-06-22 16:34:07 -0700

    update for today

--- api/api.py
+++ api/api.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 # chatgpt cleaned this up, also i am prolly going to add more endpoints and make it easier to maintain, as well as mabye automate and make the website use this api to get the forecast too, which will make everything a but eassier.
 
-last_updated = "June 21st, 2026 at 8:20 PM PT"
-temp_last_updated = "June 21st, 2026 at 8:20 PM PT"
-observations_last_updated = "June 21st, 2026 at 8:20 PM PT"
+last_updated = "June 22nd, 2026 at 4:30 PM PDT"
+temp_last_updated = "June 22nd, 2026 at 4:30 PM PDT"
+observations_last_updated = "June 22nd, 2026 at 4:30 PM PDT"
 
 app = Flask(__name__)
 
@@ -12,8 +12,8 @@ app = Flask(__name__)
 def temperature():
 
     return jsonify({
-        "temperature-high-lake-oswego": 78,
-        "temperature-low-lake-oswego": 54,
+        "temperature-high-lake-oswego": 92,
+        "temperature-low-lake-oswego": 61,
         "temperature-last-updated-lake-oswego": temp_last_updated
     })
 
@@ -42,7 +42,7 @@ def last_updated():
 @app.route('/api/observations')
 def observations():
     return jsonify({
-        "observations-lake-oswego": "76 degrees Fahrenheit, Sunny.",
+        "observations-lake-oswego": "90 degrees Fahrenheit, Sunny.",
         "last-updated-observations-lake-oswego": observations_last_updated
     })
 
