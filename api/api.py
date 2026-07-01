e87139cc15eb7df91a095163435f97ab8de434e0
webbrowser11 <terpstragraham@gmail.com>
2026-07-01 11:45:02 -0700

    Automated API update

--- api/api.py
+++ api/api.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-last_updated = "July 01, 2026 at 11:30 AM PT"
+last_updated = "July 01, 2026 at 11:45 AM PT"
 
 @app.route('/api/temperature')
 def temperature():
-    return jsonify({"high": "69 degrees fahrenheit", "low": "51 degrees fahrenheit", "last_updated": last_updated})
+    return jsonify({"high": "70 degrees fahrenheit", "low": "53 degrees fahrenheit", "last_updated": last_updated})
 
 @app.route('/api/skies')
 def skies():
@@ -17,7 +17,7 @@ def last_updated():
 
 @app.route('/api/observations')
 def observations():
-    return jsonify({"current": "69 degrees fahrenheit degrees, Partly Sunny", "last_updated": last_updated})
+    return jsonify({"current": "70 degrees fahrenheit degrees, Partly Sunny", "last_updated": last_updated})
 
 # IMPORTANT FOR VERCEL
 app = app
