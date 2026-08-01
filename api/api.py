5c190c2fac6b63cb07bf800679861459d91784cb
webbrowser11 <terpstragraham@gmail.com>
2026-08-01 16:15:02 -0700

    Automated API update

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-LAST_UPDATED = "August 01, 2026 at 04:00 PM PT"
+LAST_UPDATED = "August 01, 2026 at 04:15 PM PT"
 
 
 @app.route('/api/temperature')
@@ -17,7 +17,7 @@ def temperature():
 @app.route('/api/skies')
 def skies():
     return jsonify({
-        "skies": "Partly Sunny"
+        "skies": "Mostly Sunny"
     })
 
 
@@ -34,7 +34,7 @@ def forecast():
     return jsonify({
         "high": "76 degrees fahrenheit",
         "low": "55 degrees fahrenheit",
-        "skies": "Partly Sunny",
+        "skies": "Mostly Sunny",
         "last_updated": LAST_UPDATED
     })
 
@@ -42,7 +42,7 @@ def forecast():
 @app.route('/api/observations')
 def observations():
     return jsonify({
-        "current": "76 degrees fahrenheit, Partly Sunny",
+        "current": "76 degrees fahrenheit, Mostly Sunny",
         "last_updated": LAST_UPDATED
     })
 
